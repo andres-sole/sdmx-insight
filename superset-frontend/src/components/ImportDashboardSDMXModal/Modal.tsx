9fb794099312bb0f23852e175c8059d3fa46fc08
andres.sole <andres.sole@meaningfuldata.eu>
2023-09-18 10:33:06 +0200

    fix front-end: major revamp of the modals

--- superset-frontend/src/components/ImportDashboardSDMXModal/Modal.tsx
+++ superset-frontend/src/components/ImportDashboardSDMXModal/Modal.tsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import './Modal.css'; // Assuming you place your CSS in this file
 import { SupersetClient } from '@superset-ui/core';
-import { Upload, Space } from 'antd';
-import { set } from 'lodash';
+import { Upload, Space, Select } from 'antd';
+import Modal from 'src/components/Modal';
 import { useToasts } from 'src/components/MessageToasts/withToasts';
-import { Input } from '../Input';
+import { FormLabel } from 'src/components/Form';
 import Button from '../Button';
 
-const Modal = ({ isOpen, onClose }) => {
+const SdmxDashboardModal = ({ isOpen, onClose }) => {
+  const [sdmxFile, setSdmxFile] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [locale, setLocale] = useState('en');
+  const { addDangerToast } = useToasts();
+
   if (!isOpen) {
     return null;
   }
-
-  const [sdmxFile, setSdmxFile] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const { addDangerToast } = useToasts();
 
   const onUpload = () => {
     const formData = new FormData();
     setIsLoading(true);
     formData.append('file', sdmxFile[0]);
+    formData.append('locale', locale);
 
     SupersetClient.post({
       endpoint: 'api/v1/sdmx/dashboard',
@@ -31,6 +32,7 @@ const Modal = ({ isOpen, onClose }) => {
         window.location.reload();
       })
       .catch(err => {
+        console.log(err);
         setIsLoading(false);
         addDangerToast('There was an error loading the SDMX file');
         setSdmxFile([]);
@@ -38,54 +40,76 @@ const Modal = ({ isOpen, onClose }) => {
   };
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <h3>Import SDMX Dashboard YAML definition</h3>
-        <p style={{ color: 'rgb(75, 75, 75)' }}>
-          A sample YAML file can be downloaded{' '}
-          <a href="https://sdmx.org/wp-content/uploads/ExampleDashboardILOSTAT-SDMXHackathon-v1.1.yaml.7z">
-            here
-          </a>
-          . This is a proof of concept of what a declaration of graphs through
-          configuration files may look like.{' '}
-          <a href="https://www.sdmx2023.org/hackathon" target="_blank">
-            Learn more about this project
-          </a>
-        </p>
+    <Modal
+      show={isOpen}
+      title="Import SDMX Dashboard YAML definition"
+      onHide={onClose}
+      footer={
+        <>
+          <Button
+            onClick={onUpload}
+            buttonStyle="primary"
+            disabled={!sdmxFile.length}
+            loading={isLoading}
+          >
+            {isLoading ? 'Loading...' : 'Upload'}
+          </Button>
+        </>
+      }
+    >
+      <p style={{ color: 'rgb(75, 75, 75)' }}>
+        A sample YAML file can be downloaded{' '}
+        <a href="https://sdmx.org/wp-content/uploads/ExampleDashboardILOSTAT-SDMXHackathon-v1.1.yaml.7z">
+          here
+        </a>
+        . This is a proof of concept of what a declaration of graphs through
+        configuration files may look like.{' '}
+        <a href="https://www.sdmx2023.org/hackathon" target="_blank">
+          Learn more about this project
+        </a>
+      </p>
+      <div>
+        <Space direction="vertical">
+        <FormLabel>Load YAML Dashboard file</FormLabel>
+          <Upload
+            multiple={false}
+            accept=".yaml"
+            fileList={sdmxFile}
+            beforeUpload={value => {
+              setSdmxFile([value]);
+              return false;
+            }}
+          >
+            <Button>Select file</Button>
+          </Upload>
 
-        <div className="file-uploader">
-          <Space direction="vertical">
-            <Upload
-              multiple={false}
-              accept=".yaml"
-              fileList={sdmxFile}
-              beforeUpload={value => {
-                setSdmxFile([value]);
-                return false;
-              }}
-            >
-              <Button>Select file</Button>
-            </Upload>
-            <Button
-              onClick={onUpload}
-              buttonStyle="primary"
-              disabled={!sdmxFile.length}
-              loading={isLoading}
-            >
-              {isLoading ? 'Loading...' : 'Upload'}
-            </Button>
-          </Space>
-        </div>
-        <Button onClick={onClose}>Cancel</Button>
+
+          <FormLabel>Select Locale</FormLabel>
+          <Select
+            value={locale}
+            onChange={value => setLocale(value)}
+            options={[
+              {
+                label: 'English',
+                value: 'en',
+              },
+              {
+                label: 'French',
+                value: 'fr',
+              },
+              { label: 'Spanish', value: 'es' },
+            ]}
+          />
+        </Space>
       </div>
-    </div>
+    </Modal>
   );
 };
 
-Modal.propTypes = {
+SdmxDashboardModal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func,
   children: PropTypes.node,
 };
 
-export default Modal;
+export default SdmxDashboardModal;
