494bc9872e37fb738a9cab3595f2f7d1e556fc28
andres.sole <andres.sole@meaningfuldata.eu>
2023-09-27 13:31:38 +0200

    fix back-end: loading labels when only one locale

--- superset/sdmx.py
+++ superset/sdmx.py
@@ -16,6 +16,7 @@ from superset import db
 import json
 import re
 import sqlite3
+from sdmxthon.api.api import get_supported_agencies
 
 
 def load_database(sdmx_url, dataset_instance=None):
@@ -26,16 +27,12 @@ def load_database(sdmx_url, dataset_instance=None):
 
     data = list(message.payload.values())[0].data
 
-    if agency_id == "BIS":
-        ws = webservices.BisWs()
-    elif agency_id == "ECB":
-        ws = webservices.EcbWs()
-    elif agency_id == "ESTAT":
-        ws = webservices.EuroStatWs()
-    elif agency_id == "ILO":
-        ws = webservices.IloWs()
+    supported_agencies = get_supported_agencies()
+    if agency_id in supported_agencies.keys():
+        ws = supported_agencies[agency_id]()
+    else:
+        raise Exception("Agency not supported")
     try:
-        
         metadata = ws.get_data_flow(dataflow_id, references="descendants")
     except Exception as e:
         raise Exception(e, dataflow_id)
@@ -397,53 +394,47 @@ def create_charts(spec, datasets, dashboard, locale="en"):
 
         db.session.commit()
 
-
-def create_codelist_dataframe(codelist, concept_name):
+def _create_codelist_dataframe(codelist, concept_name):
     codelist_list = []
     for id, code in codelist.items.items():
-        item = {"id": id}
-        if isinstance(code.name, dict) : 
+        item = {'id': id}
+        if isinstance(code.name, dict): 
             for lang_code, strings in code.name.items():
                 item[f"{concept_name}-{lang_code}"] = strings["content"]
-
+        else:
+            item[f"{concept_name}-en"] = code.name
         codelist_list.append(item)
 
     return pd.DataFrame(codelist_list)
 
 
-def generate_insight_dict(metadata_payload):
+def _generate_insight_dict(metadata_payload):
     result = {}
-    codelists = metadata_payload["Codelists"]
-    structure = metadata_payload["DataStructures"]
-    if len(structure) != 1:
-        raise Exception("One structure expected")
+    codelists = metadata_payload['Codelists']
+    structure = metadata_payload['DataStructures']
+
+    if len(structure) !=  1:
+        raise Exception('One structure expected')
     structure = list(structure.values())[0]
     for id, component in structure.dimension_descriptor.components.items():
-        codelist = component.representation.codelist
+        codelist=component.representation.codelist
         if codelist:
-            codelist = create_codelist_dataframe(codelist, component.id)
-        result[id] = {"name": component.concept_identity.name, "codelist": codelist}
+            codelist = _create_codelist_dataframe(codelist, component.id)
+        result[id] = {'name': component.concept_identity.name, 'codelist': codelist}
     return result
 
 
 def generate_final_df_and_concepts_name(data, metadata_payload):
-    insight_dict = generate_insight_dict(metadata_payload)
+    insight_dict = _generate_insight_dict(metadata_payload)
+    
     concepts_names = {}
     for code, component in insight_dict.items():
-        concepts_names[code] = component["name"]
-        if component["codelist"] is not None:
-            data = data.merge(
-                component["codelist"],
-                left_on=code,
-                right_on="id",
-                how="inner",
-                suffixes=("", f"_{code}"),
-            )
-            to_drop = [column for column in data.columns if column.startswith(f"id_")]
-            data.drop(columns=to_drop, inplace=True, errors="ignore")
+        concepts_names[code] = component['name']
+        if component['codelist'] is not None:
+            data = data.merge(component['codelist'], left_on=code, right_on='id', how='inner')
+            data.drop(columns=['id_x', 'id_y'], inplace=True, errors='ignore')
 
     return data, concepts_names
-
 
 def get_dataflow_id(sdmx_url):
     full_dataflow_id = sdmx_url.split("data/")[1].split("/")[0]
@@ -463,3 +454,7 @@ def get_agency_id(sdmx_url):
         return "ESTAT"
     elif "ilo.org" in sdmx_url:
         return "ILO"
+    elif "oecd.org" in sdmx_url:
+        return "OECD"
+    elif "unicef.org" in sdmx_url:
+        return "UNICEF"    
